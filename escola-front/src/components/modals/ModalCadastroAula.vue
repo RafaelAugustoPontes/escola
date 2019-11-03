1ef533a7906d609c49dda0e0f65a923c0a150e15
Rafael Augusto <rafaelpontes13@hotmail.com>
2019-11-03 10:21:50 -0300

    Melhorias e modal para pesquisar alunos

--- escola-front/src/components/modals/ModalCadastroAula.vue
+++ escola-front/src/components/modals/ModalCadastroAula.vue
@@ -13,34 +13,46 @@
     >
       <loading :active.sync="isLoading" :can-cancel="false" :is-full-page="true"></loading>
       <form ref="form" @submit.stop.prevent="handleSubmit">
-        <b-form-group label="Turma" label-for="turma">
-          <span id="turma" class="label">{{aula.turma.nome}}</span>
-        </b-form-group>
-        <b-form-group label="Curso" label-for="curso">
-          <span id="curso" class="label">{{aula.turma.curso.nome}}</span>
-        </b-form-group>
-        <b-form-group label="Unidade" label-for="unidade">
-          <span id="unidade" class="label">{{aula.turma.unidade.nome}}</span>
-        </b-form-group>
-        <b-form-group label="Estágio" label-for="estagio">
-          <span id="estagio" class="label">{{aula.turma.estagio.nome}}</span>
-        </b-form-group>
-        <b-form-group label="Data da aula" label-for="data-aula">
-          <b-form-input id="dataNascimento" type="date" v-model="aula.data" required></b-form-input>
-        </b-form-group>
-        <b-form-group label="Diário da aula" label-for="diario">
-          <b-form-textarea id="diario" v-model="aula.diario" rows="3" maxlength="2000"></b-form-textarea>
-        </b-form-group>
-        <b-card-group deck>
-          <b-card header="Alunos que faltaram">
-            <tabela-generica :itens="aula.turma.alunos" :campos="campos" @selecionar="selecionar"></tabela-generica>
-          </b-card>
-        </b-card-group>
-        <b-card-group deck>
-          <b-card header="Alunos presentes">
-            <tabela-generica :itens="aula.alunos" :campos="campos" @editar="remover"></tabela-generica>
-          </b-card>
-        </b-card-group>
+        <b-card no-body>
+          <b-tabs pills card>
+            <b-tab title="Dados da aula" active>
+              <b-card-text>
+                <b-form-group label="Turma" label-for="turma">
+                  <span id="turma" class="label">{{aula.turma.nome}}</span>
+                </b-form-group>
+                <b-form-group label="Curso" label-for="curso">
+                  <span id="curso" class="label">{{aula.turma.curso.nome}}</span>
+                </b-form-group>
+                <b-form-group label="Unidade" label-for="unidade">
+                  <span id="unidade" class="label">{{aula.turma.unidade.nome}}</span>
+                </b-form-group>
+                <b-form-group label="Estágio" label-for="estagio">
+                  <span id="estagio" class="label">{{aula.turma.estagio.nome}}</span>
+                </b-form-group>
+                <b-form-group label="Data da aula" label-for="data-aula">
+                  <b-form-input id="dataNascimento" type="date" v-model="aula.data" required></b-form-input>
+                </b-form-group>
+                <b-form-group label="Diário da aula" label-for="diario">
+                  <b-form-textarea id="diario" v-model="aula.diario" rows="3" maxlength="2000"></b-form-textarea>
+                </b-form-group>
+              </b-card-text>
+            </b-tab>
+            <b-tab title="Alunos presentes">
+              <b-button v-b-modal.modal-pesquisa-aluno variant="info">
+                <i class="fas fa-search"></i>
+                Procurar
+              </b-button>
+              <div v-for="aluno in aula.alunos" :value="aluno.idPessoa" :key="aluno.idPessoa">
+                <chip :title="aluno.nome" :id="aluno.idPessoa" @close="remover"></chip>
+              </div>
+              <modal-pesquisa-aluno
+                ref="modalPesquisa"
+                :alunos="aula.turma.alunos"
+                @alunoSelecionado="selecionar"
+              ></modal-pesquisa-aluno>
+            </b-tab>
+          </b-tabs>
+        </b-card>
       </form>
     </b-modal>
   </div>
@@ -50,6 +62,8 @@
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import TabelaGenerica from '../tables/TabelaGenerica';
+import ModalPesquisaAluno from './modal-pesquisa-aluno/modal-pesquisa-aluno.vue';
+import Chip from '../partials/chip/chip.vue';
 
 export default {
   props: ['aula'],
@@ -64,6 +78,8 @@ export default {
   components: {
     'tabela-generica': TabelaGenerica,
     Loading,
+    'modal-pesquisa-aluno': ModalPesquisaAluno,
+    chip: Chip,
   },
 
   methods: {
@@ -76,10 +92,10 @@ export default {
       });
       if (!alunoExiste) this.aula.alunos.unshift(item);
     },
-    remover(item) {
+    remover(id) {
       let posicao = 0;
       for (let i = 0; i < this.aula.alunos.length; i++) {
-        if (this.aula.alunos[i].idPessoa == item.idPessoa) {
+        if (this.aula.alunos[i].idPessoa == id) {
           this.aula.alunos.splice(i, 1);
         }
       }
--- escola-front/src/components/modals/modal-pesquisa-aluno/modal-pesquisa-aluno.js
+++ escola-front/src/components/modals/modal-pesquisa-aluno/modal-pesquisa-aluno.js
@@ -1,13 +1,29 @@
+import TabelaGenerica from '../../tables/TabelaGenerica.vue';
+
 export default {
   props: ['alunos'],
+
+  components: {
+    TabelaGenerica,
+  },
 
   data() {
     return {
       campos: ['nome'],
+      exibirFooter: false,
+      filtro: '',
     };
   },
 
   methods: {
+    dadosComFiltro() {
+      if (this.filtro) {
+        let expressao = new RegExp(this.filtro.trim(), 'i');
+        return this.alunos.filter(pessoa => expressao.test(pessoa.nome));
+      } else {
+        return this.alunos;
+      }
+    },
     selecionar(aluno) {
       this.$emit('alunoSelecionado', aluno);
       this.fechar();
